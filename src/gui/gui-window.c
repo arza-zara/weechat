94921b35d3914fc4a619546a0672edee2f6a9e6c
arza <arza@arza.us>
2017-03-25 08:46:22 +0200

    core: fix searching when there are matches only below the starting position

--- src/gui/gui-window.c
+++ src/gui/gui-window.c
@@ -1570,13 +1570,16 @@ gui_window_search_text (struct t_gui_window *window)
     if (!window)
         return 0;
 
-    if (window->buffer->text_search == GUI_TEXT_SEARCH_BACKWARD)
+    if (window->buffer->text_search == GUI_TEXT_SEARCH_BACKWARD
+        || window->buffer->text_search == GUI_TEXT_SEARCH_EITHER)
     {
         if (window->buffer->lines->first_line
             && window->buffer->input_buffer && window->buffer->input_buffer[0])
         {
             ptr_line = (window->scroll->start_line) ?
-                gui_line_get_prev_displayed (window->scroll->start_line) :
+                ((window->buffer->text_search == GUI_TEXT_SEARCH_BACKWARD) ?
+                    gui_line_get_prev_displayed (window->scroll->start_line) :
+                    window->scroll->start_line) :
                 gui_line_get_last_displayed (window->buffer);
             while (ptr_line)
             {
@@ -1593,13 +1596,16 @@ gui_window_search_text (struct t_gui_window *window)
             }
         }
     }
-    else if (window->buffer->text_search == GUI_TEXT_SEARCH_FORWARD)
+    if (window->buffer->text_search == GUI_TEXT_SEARCH_FORWARD
+        || window->buffer->text_search == GUI_TEXT_SEARCH_EITHER)
     {
         if (window->buffer->lines->first_line
             && window->buffer->input_buffer && window->buffer->input_buffer[0])
         {
             ptr_line = (window->scroll->start_line) ?
-                gui_line_get_next_displayed (window->scroll->start_line) :
+                ((window->buffer->text_search == GUI_TEXT_SEARCH_FORWARD) ?
+                    gui_line_get_next_displayed (window->scroll->start_line) :
+                    window->scroll->start_line) :
                 gui_line_get_first_displayed (window->buffer);
             while (ptr_line)
             {
@@ -1634,7 +1640,7 @@ gui_window_search_start (struct t_gui_window *window,
     window->scroll->text_search_start_line = text_search_start_line;
     window->buffer->text_search =
         (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
-        GUI_TEXT_SEARCH_BACKWARD : GUI_TEXT_SEARCH_FORWARD;
+        GUI_TEXT_SEARCH_EITHER : GUI_TEXT_SEARCH_FORWARD;
 
     if ((window->buffer->text_search_where == 0)
         ||  CONFIG_BOOLEAN(config_look_buffer_search_force_default))
@@ -1691,7 +1697,7 @@ gui_window_search_restart (struct t_gui_window *window)
     window->scroll->start_line_pos = 0;
     window->buffer->text_search =
         (window->buffer->type == GUI_BUFFER_TYPE_FORMATTED) ?
-        GUI_TEXT_SEARCH_BACKWARD : GUI_TEXT_SEARCH_FORWARD;
+        GUI_TEXT_SEARCH_EITHER : GUI_TEXT_SEARCH_FORWARD;
     window->buffer->text_search_found = 0;
     gui_input_search_compile_regex (window->buffer);
     if (gui_window_search_text (window))
